3d35e9236b7c0293766ac0d3be1dc995b6ce6381
Alec Armbruster <alecsamuelarmbruster@gmail.com>
2020-01-06 19:52:15 -0800

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,28 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <View/>
 </template>
-
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import View from '@/components/View.vue'
 export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  }
+    name: 'app',
 }
 </script>
-
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+@import url("https://rsms.me/inter/inter.css");
+
+html {
+    font-family: "Inter", sans-serif;
+}
+
+@supports (font-variation-settings: normal) {
+    html {
+        font-family: "Inter var", sans-serif;
+    }
+}
+
+* {
+    font-family: "Inter", sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
 }
 </style>
